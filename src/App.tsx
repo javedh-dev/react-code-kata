b5d39610180de3f0096a973e731117caab0f099d
Javed Hussain <javedh.dev@gmail.com>
2023-09-05 23:08:45 +0530

    Added support for tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,20 @@
 import React from "react";
 import "./App.scss";
-import logo from "./logo.svg";
+import logo from "../public/logo.svg";
 
 const App = () => {
-	return (
-		<div className='app'>
-			<img src={logo} className='App-logo' alt='logo' />
-			<h1>React Coding Kata!!!</h1>
-			<h4>Author - Javed Hussain</h4>
-			<p>
-				This project is setup using <span>React</span>, <span>Typescript</span>,{" "}
-				<span>Webpack</span>, <span>Babel</span> and <span>Scss</span>
-			</p>
-		</div>
-	);
+  return (
+    <div className="app">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="title">React Coding Kata!!!</h1>
+      <h4>Author - Javed Hussain</h4>
+      <p>
+        This project is setup using <span>React</span>, <span>Typescript</span>,{" "}
+        <span>Webpack</span>, <span>Babel</span>, <span>Tailwind</span> and{" "}
+        <span>Scss</span>
+      </p>
+    </div>
+  );
 };
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -2,46 +2,64 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
 
-module.exports = {
+module.exports = (env, argv) => {
+  const mode = argv.mode || "development";
+  const config = {
     entry: "./src/index.tsx",
+    devtool: mode === "development" ? "inline-source-map" : false,
+    mode: mode,
+    performance: {
+      hints: process.env.NODE_ENV === "production" ? "warning" : false,
+    },
     output: {
-        filename: "bundle.[hash].js",
-        path: path.resolve(__dirname, "dist"),
+      filename: "bundle.[hash].js",
+      path: path.resolve(__dirname, "dist"),
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: "./public/index.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "bundle.[hash].css"
-        }),
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+      }),
+      new MiniCssExtractPlugin({
+        filename: "bundle.[hash].css",
+      }),
     ],
     resolve: {
-        modules: [__dirname, "src", "node_modules"],
-        extensions: ["*", ".js", ".jsx", ".tsx", ".ts", ".scss"],
+      modules: [__dirname, "src", "node_modules"],
+      extensions: [".*", ".js", ".jsx", ".tsx", ".ts", ".scss", ".json"],
+      alias: {
+        "@": path.resolve(__dirname, "src/"),
+        "@public": path.resolve(__dirname, "public/"),
+      },
     },
     module: {
-        rules: [
-            {
-                test: /\.(t|j)sx?$/,
-                exclude: /node_modules/,
-                use: ["babel-loader", "ts-loader"],
-            },
-            {
-                test: /\.s(a|c)ss$/,
-                exclude: /node_modules/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-            },
-            {
-                test: /\.(png|svg|jpg|gif|jpeg)$/,
-                exclude: /node_modules/,
-                use: ["file-loader"],
-            },
-        ],
+      rules: [
+        {
+          test: /\.(t|j)sx?$/,
+          exclude: /node_modules/,
+          use: ["babel-loader", "ts-loader"],
+        },
+        {
+          test: /\.s(a|c)ss$/,
+          exclude: /node_modules/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader",
+            "postcss-loader",
+            "sass-loader",
+          ],
+        },
+        {
+          test: /\.(png|svg|jpg|gif|jpeg)$/,
+          exclude: /node_modules/,
+          use: ["file-loader"],
+        },
+      ],
     },
     devServer: {
-        port: 3000,
-        compress: true,
-
-    }
+      port: 3000,
+      compress: true,
+      historyApiFallback: true,
+    },
+  };
+  return config;
 };
